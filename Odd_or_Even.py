c932e0187d7a848f49c2e6f525f7f8c7dd4f81e4
Akk525 <63389394+Akk525@users.noreply.github.com>
2023-07-27 20:55:31 +0530

    Update Odd_or_Even.py

--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -1,6 +1,10 @@
+# Get user input for the number to be checked.
 number = int(input("Which number do you want to check? "))
 
+# Check if the number is even by dividing it by 2 and checking if the remainder is 0.
 if number % 2 == 0:
+    # If the remainder is 0, then the number is even.
     print("This is an even number.")
 else:
+    # If the remainder is not 0, then the number is odd.
     print("This is an odd number.")
